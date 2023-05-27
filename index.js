71504ac37a419832cde172bbfc3e33481fa020e2
Bhumika Agarwal <f20220946@pilani.bits-pilani.ac.in>
2023-05-27 21:52:54 +0530

    feat:added dark mode functionality and changed ui

--- index.js
+++ index.js
@@ -40,6 +40,18 @@ const scrollinglr = () => {
   const scroll2 = document.querySelector(".font-flex2");
   const scroll3 = document.querySelector(".font-flex3");
   const scroll4 = document.querySelector(".font-flex4");
+  var image1 = document.querySelector(".moving-img1");
+  var image2 = document.querySelector(".moving-img2");
+  var image3 = document.querySelector(".moving-img3");
+  var image4 = document.querySelector(".moving-img4");
+  var image5 = document.querySelector(".moving-img5");
+  var image6 = document.querySelector(".moving-img6");
+  var image7 = document.querySelector(".moving-img7");
+  var image8 = document.querySelector(".moving-img8");
+
+  let scrollY = window.scrollY;
+
+  // font page scrolling
 
   if (
     document.querySelector(".font-flex2").getBoundingClientRect().top != 100
@@ -52,7 +64,9 @@ const scrollinglr = () => {
   if (
     document.querySelector(".font-flex1").getBoundingClientRect().top != 100
   ) {
-    let val3 = document.querySelector(".font-flex1").getBoundingClientRect().top;
+    let val3 = document
+      .querySelector(".font-flex1")
+      .getBoundingClientRect().top;
     let val4 = val3 * 0.1;
     scroll1.style.transform = `translateY(${val4}px)`;
   }
@@ -60,7 +74,9 @@ const scrollinglr = () => {
   if (
     document.querySelector(".font-flex3").getBoundingClientRect().top != 100
   ) {
-    let val5 = document.querySelector(".font-flex3").getBoundingClientRect().top;
+    let val5 = document
+      .querySelector(".font-flex3")
+      .getBoundingClientRect().top;
     let val6 = val5 * 0.2;
     scroll3.style.transform = `translateY(${val6}px)`;
   }
@@ -68,9 +84,161 @@ const scrollinglr = () => {
   if (
     document.querySelector(".font-flex4").getBoundingClientRect().top != 100
   ) {
-    let val7 = document.querySelector(".font-flex4").getBoundingClientRect().top;
+    let val7 = document
+      .querySelector(".font-flex4")
+      .getBoundingClientRect().top;
     let val8 = val7 * 0.3;
     scroll4.style.transform = `translateY(${val8}px)`;
   }
+
+  // circle rotation
+
+  if (scrollY % 100 < 50) {
+    document.querySelector(".font-circle").style.transform = "rotate(1deg)";
+  } else {
+    document.querySelector(".font-circle").style.transform = "rotate(-1deg)";
+  }
+
+  // moving-image
+  if (
+    document.querySelector(".container1-img").getBoundingClientRect().top != 100
+  ) {
+    let val9 =
+      document.querySelector(".container1-img").getBoundingClientRect().top -
+      100;
+    let val10 = val9 * 0.1;
+    image1.style.transform = `translateY(${val10}px)`;
+  }
+
+  if (
+    document.querySelector(".container2-img").getBoundingClientRect().top != 100
+  ) {
+    console.log(
+      document.querySelector(".container2-img").getBoundingClientRect().top
+    );
+    let val11 =
+      document.querySelector(".container2-img").getBoundingClientRect().top -
+      1000;
+    let val12 = val11 * 0.1;
+    image2.style.transform = `translateY(${val12}px)`;
+  }
+
+  if (
+    document.querySelector(".container3-img").getBoundingClientRect().top != 100
+  ) {
+    console.log(
+      document.querySelector(".container3-img").getBoundingClientRect().top
+    );
+    let val13 =
+      document.querySelector(".container3-img").getBoundingClientRect().top -
+      1000;
+    let val14 = val13 * 0.1;
+    image3.style.transform = `translateY(${val14}px)`;
+  }
+
+  if (
+    document.querySelector(".container4-img").getBoundingClientRect().top != 100
+  ) {
+    console.log(
+      document.querySelector(".container4-img").getBoundingClientRect().top
+    );
+    let val15 =
+      document.querySelector(".container4-img").getBoundingClientRect().top -
+      1000;
+    let val16 = val15 * 0.1;
+    image4.style.transform = `translateY(${val16}px)`;
+  }
+
+  if (
+    document.querySelector(".container5-img").getBoundingClientRect().top != 100
+  ) {
+    console.log(
+      document.querySelector(".container5-img").getBoundingClientRect().top
+    );
+    let val17 =
+      document.querySelector(".container5-img").getBoundingClientRect().top -
+      1000;
+    let val18 = val17 * 0.1;
+    image5.style.transform = `translateY(${val18}px)`;
+  }
+
+  if (
+    document.querySelector(".font-use-bottom-img").getBoundingClientRect()
+      .top != 100
+  ) {
+    console.log(
+      document.querySelector(".font-use-bottom-img").getBoundingClientRect().top
+    );
+    let val19 =
+      document.querySelector(".font-use-bottom-img").getBoundingClientRect()
+        .top - 1000;
+    let val20 = val19 * 0.1;
+    image6.style.transform = `translateY(${val20}px)`;
+  }
+
+  if (
+    document.querySelector(".font-use-bottom-img").getBoundingClientRect()
+      .top != 100
+  ) {
+    console.log(
+      document.querySelector(".font-use-bottom-img").getBoundingClientRect().top
+    );
+    let val21 =
+      document.querySelector(".font-use-bottom-img").getBoundingClientRect()
+        .top - 1000;
+    let val22 = val21 * 0.1;
+    image7.style.transform = `translateY(${val22}px)`;
+  }
+
+  if (
+    document.querySelector(".font-use-bottom-img").getBoundingClientRect()
+      .top != 100
+  ) {
+    console.log(
+      document.querySelector(".font-use-bottom-img").getBoundingClientRect().top
+    );
+    let val23 =
+      document.querySelector(".font-use-bottom-img").getBoundingClientRect()
+        .top - 500;
+    let val24 = val23 * 0.09;
+    image8.style.transform = `translateY(${val24}px)`;
+  }
 };
 window.addEventListener("scroll", scrollinglr);
+
+// on hover cards
+
+window.onload = function (e) {
+  var s = document.querySelectorAll(".font-wrapper");
+  var p = document.querySelectorAll(".font-box");
+  var q = document.querySelectorAll(".font-hidden");
+
+  console.log(s);
+
+  for (let i = 0; i < s.length; i++) {
+    s[i].addEventListener("mouseover", function () {
+      console.log(`Text = ${s[i].innerText}`);
+      s[i].style.color = "black";
+      s[i].style.backgroundColor = "white";
+      p[i].style.display = "none";
+      q[i].style.display = "block";
+    });
+  }
+  for (let i = 0; i < s.length; i++) {
+    s[i].addEventListener("mouseout", function () {
+      console.log(`Text = ${s[i].innerText}`);
+      s[i].style.color = "white";
+      s[i].style.backgroundColor = "black";
+      p[i].style.display = "block";
+      q[i].style.display = "none";
+    });
+  }
+};
+
+// dark mode
+var mode = document.querySelector(".mode");
+function darkmode() {
+  var lightmode = document.querySelector(".darkmode-ele");
+  lightmode.classList.toggle("darkbody");
+  mode.textContent = "Dark Mode";
+}
